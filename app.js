1e9ce35dafed90eae28e9369166b13126299e2b6
WalkerM-SNHU <walker.martin@snhu.edu>
2023-12-07 16:08:54 -0500

    Secured API endpoints with passport authentication

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+require("dotenv").config();
+
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const hbs = require("hbs");
+const passport = require("passport");
+
 require("./app_api/database/db");
+
+require("./app_api/config/passport");
 
 var indexRouter = require("./app_server/routes/index");
 var usersRouter = require("./app_server/routes/users");
@@ -19,26 +25,25 @@ const apiRouter = require("./app_api/routes/index");
 
 var app = express();
 
-
 // view engine setup
 app.set("views", path.join(__dirname, "app_server", "views"));
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 hbs.registerPartials(path.join(__dirname, "app_server", "views/partials"));
 app.set("view engine", "hbs");
 
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(passport.initialize());
 
 // Allow CORS
 app.use("/api", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
@@ -54,6 +59,12 @@ app.use("/contact", contactRouter);
 app.use("/about", aboutRouter);
 
 app.use("/api", apiRouter);
+
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    res.status(401).json({ message: err.name + ": " + err.message });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
+import { Http, Headers } from "@angular/http";
 
 import { Trip } from "../models/trip";
 
@@ -29,19 +29,26 @@ export class TripDataService {
   }
 
   public addTrip(formData: Trip): Promise<Trip> {
-    console.log("Insided TripDataService#addTrip");
+    console.log("Inside TripDataService#addTrip");
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("travlr-token")}`,
+    });
     return this.http
-      .post(this.tripUrl, formData)
+      .post(this.tripUrl, formData, { headers: headers })
       .toPromise()
       .then((response) => response.json() as Trip[])
       .catch(this.handleError);
   }
 
-  public updateTrip(formData: Trip): Promise<Trip[]> {
+  public updateTrip(formData: Trip): Promise<Trip> {
     console.log("Inside TripDataService#updateTrip");
-    console.log(formData);
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("travlr-token")}`,
+    });
     return this.http
-      .put(this.tripUrl + formData.code, formData)
+      .put(this.tripUrl + formData.code, formData, { headers: headers })
       .toPromise()
       .then((response) => response.json() as Trip[])
       .catch(this.handleError);
